0c0c8c5a6a5a229aa05a9abd2622bdb368f97a1d
Henry O. <devodehenry.2000@gmail.com>
2025-02-19 20:19:53 +0100

    fixed vercel.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Ensures correct paths in production
+  build: {
+    outDir: "dist", // Ensure Vite builds to the correct folder
+  },
   server: {
     proxy: {
-      "/api": {
+      "/": {
         target: "http://localhost:3000",
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+        secure: false,
+      },
+    },
+  },
 });
