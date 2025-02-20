3b375cf9fe5107f04c3fe023f2cc43d6a6254870
Henry O. <devodehenry.2000@gmail.com>
2025-02-21 00:58:55 +0100

    week 8

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
 import NotesApp from './components/NotesApp';
 import WeekSix from './components/Week6';
 import Apps from './components/week7'
@@ -30,16 +31,18 @@ function Home() {
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>Click the button below to go to the Notes App.</p>
+      <p>Made use of Material UI for the button #week 8</p>
+      <br />
       <Link to="/notes">
-        <button>Go to Notes App</button>
+        <Button variant="contained">Go to Notes App</Button>
       </Link> 👈 Week 5 task
         <br />
       <Link to="/week6">
-        <button>Go to Notes App</button>
+        <Button variant="contained">User Auth Form App</Button>
       </Link> 👈 Week 6 task
       <br />
       <Link to="/week7">
-        <button>Go to Notes App</button>
+        <Button variant="contained">Counter App</Button>
       </Link> 👈 Week 7 task
     </div>
   );
